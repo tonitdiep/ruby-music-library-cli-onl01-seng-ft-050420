e82a5d7b9296252fc07620811cfc5299ab332295
Toni  Diep <adiep123@gmail.com>
2020-05-24 00:02:13 +0000

    Done.

--- lib/Song.rb
+++ lib/Song.rb
@@ -5,6 +5,7 @@ class Song
     @name = name
     @@all << self
     @artist = artist if artist
+    @artist = artist 
   end
   
   def self.all
@@ -26,6 +27,6 @@ class Song
   end
   def artist=(artist)
     @artist = artist
-    @artist << (add_song)
+    artist.add_song(self)
   end
 end
