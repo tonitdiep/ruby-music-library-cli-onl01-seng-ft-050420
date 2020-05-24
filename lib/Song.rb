d691b1e54efacd829487d526392bd5bc34083734
Toni  Diep <adiep123@gmail.com>
2020-05-24 03:22:09 +0000

    Done.

--- lib/Song.rb
+++ lib/Song.rb
@@ -4,7 +4,7 @@ class Song
   @@all = []
   def initialize(name, artist = nil, genre = nil)
     @name = name
-    @@all << self
+    # @@all << self
     self.artist = artist if artist
     self.genre = genre if genre
   end
