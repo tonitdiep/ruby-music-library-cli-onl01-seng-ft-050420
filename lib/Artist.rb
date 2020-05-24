f3132aafbb3eba7b23c7500f2e1ecadd72bc8ac1
Toni  Diep <adiep123@gmail.com>
2020-05-24 06:19:56 +0000

    Done.

--- lib/Artist.rb
+++ lib/Artist.rb
@@ -3,7 +3,7 @@ class Artist
   @@all = []
   def initialize(name)
     @name = name
-    @@all << self
+    # @@all << self
     @songs = []
   end
   
--- lib/Genre.rb
+++ lib/Genre.rb
@@ -3,7 +3,7 @@ class Genre
   @@all = []
   def initialize(name)
     @name = name
-    @@all << self
+    # @@all << self
     @songs = []
     # save
   end
