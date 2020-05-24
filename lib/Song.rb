d31d131d240a19106ec7ac018d5ce3dc9518290e
Toni  Diep <adiep123@gmail.com>
2020-05-24 17:56:21 +0000

    Done.

--- lib/Song.rb
+++ lib/Song.rb
@@ -38,15 +38,15 @@ class Song
   def self.find_by_name(name)
     @@all.detect {|song| song.name == name}
   end
+  
   def self.find_or_create_by_name(name)
-    
-    # find_by_name(name).detect || create_by_name(name).detect
-    if !self.find_by_name(name)
-      find_by_name(name)
-    else !self.create_by_name(name)
-      create_by_name(name)
+    if self.find_by_name(name)
+      self.find_by_name(name)
+    elsif
+      self.create(name)
+    else
+      self.find_by_name(name)
     end
-      find_by_name(name) || create_by_name(name)
   end
   
   
