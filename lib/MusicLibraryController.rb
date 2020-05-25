402a0448268b0add62806a896a86efa9c623c027
Toni  Diep <adiep123@gmail.com>
2020-05-25 00:58:33 +0000

    Done.

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -7,17 +7,17 @@ class MusicLibraryController
   end
   
   def call
-    input = " "
-      while input != "exit"
+    input = ' '
+      while input != 'exit'
         puts "Welcome to your music library!"
-        puts "Enter List Songs."
-        puts "Enter List Artists."
-        puts "Enter List Genres."
-        puts "Enter List of Songs by Artist."
-        puts "Enter List of Songs by Genre."
-        puts "Enter Play Song."
-        puts "To quit, enter 'exit'." 
-        puts "Choose what's next in the Music Library."
+        puts "To list all of your songs, enter 'list songs'."
+        puts "To list all of the artists in your library, enter 'list artists'."
+        puts "To list all of the genres in your library, enter 'list genres'."
+        puts "To list all of the songs by a particular artist, enter 'list artist'."
+        puts "To list all of the songs of a particular genre, enter 'list genre'."
+        puts "To play a song, enter 'play song'."
+        puts "To quit, type 'exit'." 
+        puts "What would you like to do."
         input = gets.chomp
       end
   end
